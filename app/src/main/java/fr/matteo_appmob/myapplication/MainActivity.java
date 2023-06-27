f7db6951f0555031774b9503c366fab9a51efb7f
Mat <moisant.matteo@gmail.com>
2023-06-28 00:10:20 +0200

    Update shop and tuto

--- app/src/main/java/fr/matteo_appmob/myapplication/MainActivity.java
+++ app/src/main/java/fr/matteo_appmob/myapplication/MainActivity.java
@@ -1,14 +1,23 @@
 package fr.matteo_appmob.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int[] cristalTotal = {0};
+    final int[] sylvan_essenceTotal = {0};
+    final int[] currentWord = {1};
+    private String[] txt_welcome = {};
 
     public void setTotal(TextView place, int total){
         place.setText(String.valueOf(total));
@@ -22,8 +31,46 @@ public class MainActivity extends AppCompatActivity {
         int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
 
-        ImageView imgNotif = findViewById(R.id.notif);
-        imgNotif.setVisibility(ImageView.INVISIBLE);
+        txt_welcome = getResources().getStringArray(R.array.txt_welcome);
+
+        ImageView imgNotify = findViewById(R.id.notif);
+
+        TextView txtNotify = findViewById(R.id.txt_notif);
+        txtNotify.setText(txt_welcome[0]);
+
+        Thread threadTuto = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final boolean[] finish = {false};
+                while(!finish[0]){
+                    try {
+                        Thread.sleep(5000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (finish[0]){
+                                    imgNotify.setVisibility(ImageView.INVISIBLE);
+                                    txtNotify.setText("");
+                                    return;
+                                }
+                                imgNotify.setVisibility(ImageView.VISIBLE);
+                                txtNotify.setText(txt_welcome[currentWord[0]]);
+                                if(currentWord[0] >= txt_welcome.length - 1){
+                                    finish[0] = true;
+                                    currentWord[0] = 0;
+                                    return;
+                                }
+                                currentWord[0]++;
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        threadTuto.start();
 
         ImageView btnShop = findViewById(R.id.btn_shop);
         btnShop.setOnClickListener(new View.OnClickListener() {
@@ -57,12 +104,6 @@ public class MainActivity extends AppCompatActivity {
 
         TextView cristal = findViewById(R.id.txt_cristal);
         TextView sylvan_essence = findViewById(R.id.txt_sylvan_essence);
-
-        //setTotal(cristal, 1000);
-        //setTotal(sylvan_essence, 1000);
-
-        final int[] cristalTotal = {0};
-        final int[] sylvan_essenceTotal = {0};
 
         Thread thread = new Thread(new Runnable() {
             @Override
--- app/src/main/java/fr/matteo_appmob/myapplication/ShopActivity.java
+++ app/src/main/java/fr/matteo_appmob/myapplication/ShopActivity.java
@@ -1,12 +1,12 @@
 package fr.matteo_appmob.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.Group;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ShopActivity extends AppCompatActivity {
@@ -14,13 +14,31 @@ public class ShopActivity extends AppCompatActivity {
     ImageView btnMulti1;
     ImageView btnMulti10;
     ImageView btnMulti100;
+    TextView price_upgrade1;
+    TextView price_upgrade2;
 
-    final int[] selected = {1};
+    final int[] selectedMulti = {1};
 
     public void updateMulti(){
-        if(this.selected[0] != 1) this.btnMulti1.setImageResource(R.drawable.boutton_multi);
-        if(this.selected[0] != 10) this.btnMulti10.setImageResource(R.drawable.boutton_multi);
-        if(this.selected[0] != 100) this.btnMulti100.setImageResource(R.drawable.boutton_multi);
+        if(this.selectedMulti[0] != 1) this.btnMulti1.setImageResource(R.drawable.boutton_multi);
+        if(this.selectedMulti[0] != 10) this.btnMulti10.setImageResource(R.drawable.boutton_multi);
+        if(this.selectedMulti[0] != 100) this.btnMulti100.setImageResource(R.drawable.boutton_multi);
+        updatePrice();
+    }
+
+    public void updatePrice(){
+        if(this.selectedMulti[0] == 1){
+            this.price_upgrade1.setText("1000");
+            this.price_upgrade2.setText("1000");
+        }
+        else if(this.selectedMulti[0] == 10){
+            this.price_upgrade1.setText("10000");
+            this.price_upgrade2.setText("10000");
+        }
+        else if(this.selectedMulti[0] == 100){
+            this.price_upgrade1.setText("100000");
+            this.price_upgrade2.setText("100000");
+        }
     }
 
     @Override
@@ -31,6 +49,8 @@ public class ShopActivity extends AppCompatActivity {
         this.btnMulti1 = findViewById(R.id.btn_multi1);
         this.btnMulti10 = findViewById(R.id.btn_multi10);
         this.btnMulti100 = findViewById(R.id.btn_multi100);
+        this.price_upgrade1 = findViewById(R.id.price_upgrade1);
+        this.price_upgrade2 = findViewById(R.id.price_upgrade2);
         btnMulti1.setImageResource(R.drawable.boutton_multi_select);
 
         int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
@@ -47,11 +67,10 @@ public class ShopActivity extends AppCompatActivity {
         });
 
         // select multiplier shop
-
         this.btnMulti1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selected[0] = 1;
+                selectedMulti[0] = 1;
                 updateMulti();
                 btnMulti1.setImageResource(R.drawable.boutton_multi_select);
             }
@@ -61,7 +80,7 @@ public class ShopActivity extends AppCompatActivity {
         this.btnMulti10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selected[0] = 10;
+                selectedMulti[0] = 10;
                 updateMulti();
                 btnMulti10.setImageResource(R.drawable.boutton_multi_select);
             }
@@ -71,12 +90,28 @@ public class ShopActivity extends AppCompatActivity {
         this.btnMulti100.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                selected[0] = 100;
+                selectedMulti[0] = 100;
                 updateMulti();
                 btnMulti100.setImageResource(R.drawable.boutton_multi_select);
             }
         });
 
 
+        // buy upgrades
+        ImageView btnUpgrade1 = findViewById(R.id.btn_upgrade1);
+        btnUpgrade1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Arboretum amélioré !", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageView btnUpgrade2 = findViewById(R.id.btn_upgrade2);
+        btnUpgrade2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Fonderie cristaline amélioré !", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
